d7a9433533a5e9e4b586c5adee17c38d74243cfb
KrystianDe <53207745+KrystianDe@users.noreply.github.com>
2020-03-30 21:39:36 +0200

    Add project files.

---
+++ Api/Controllers/BookBorrowsController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+using Library.Models.DTO;
+using Library.Repositories.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Rollbar;
+
+
+namespace Library.Controllers
+{
+    [Route("api/book-borrows")]
+    //[Authorize(Roles="Reader")]
+    public class BookBorrowsController : Controller
+    {
+
+        private readonly ILogger<BookBorrowsController> _logger;
+
+        public BookBorrowsController(ILogger<BookBorrowsController> logger)
+        {
+            _logger = logger;
+        }
+
+        private readonly IBookBorrowRepository _bookBorrowRepository;
+
+        public BookBorrowsController(IBookBorrowRepository bookBorrowRepository)
+        {
+            _bookBorrowRepository = bookBorrowRepository;
+        }
+
+
+        [HttpPost(Name = nameof(AddBookBorrow))]
+        public async Task<IActionResult> AddBookBorrow([FromBody] BookBorrowDto borrow)
+        {
+            RollbarLocator.RollbarInstance.Error(new Exception("Błąd z Rollbar"));
+            _logger.LogError("Błąd w AddBookBorrow ");
+
+            var res = await _bookBorrowRepository.AddBookBorrow(borrow);
+            return CreatedAtRoute(nameof(AddBookBorrow), res);
+        }
+
+        [HttpPut("{idBookBorrow}")]
+        public async Task<IActionResult> UpdateBookBorrow([FromBody] UpdateBookBorrowDto borrow)
+        {
+            await _bookBorrowRepository.ChangeBookBorrow(borrow);
+            return NoContent();
+        }
+
+        
+    }
+}
